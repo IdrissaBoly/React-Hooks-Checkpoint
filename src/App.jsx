87faa3de970ba28c22ad09216ccc2f5ed4c33f7c
IdrissaBoly <idrissaboly15@gmail.com>
2023-11-05 10:49:19 +0100

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,49 @@
-import React, { useState } from 'react';
-import MovieList from './MovieList';
-import Filter from './Filter';
+import React, { useState } from "react";
+import MovieList from "./MovieList";
+import Filter from "./Filter";
 
 const App = () => {
   const [movies] = useState([
-    { title: 'Film 1', description: 'Description du film 1', posterURL: 'https://2.bp.blogspot.com/_iyzDU6gO2tY/STcukKPANaI/AAAAAAAAAEY/--NQxQFihME/s400/El+%C3%BAltimo+gran+mago.jpg', rating: 4.5 },
-    { title: 'Film 2', description: 'Description du film 2', posterURL: 'https://2.bp.blogspot.com/_iyzDU6gO2tY/STcukKPANaI/AAAAAAAAAEY/--NQxQFihME/s400/El+%C3%BAltimo+gran+mago.jpg', rating: 3.8 },
+    {
+      title: "Film 2",
+      description: "Description du film 2",
+      posterURL:
+        "https://www.cinehorizons.net/sites/default/files/affiches/1101829632-babylon.jpg",
+      rating: 3.8,
+    },
+    {
+      title: "Film 1",
+      description: "Description du film 1",
+      posterURL:
+        "https://toutelaculture.com/wp-content/uploads/2023/01/affiche-Babylone-680x924.webp",
+      rating: 4.5,
+    },
   ]);
 
   const [filteredMovies, setFilteredMovies] = useState(movies);
 
   const handleTitleChange = (event) => {
     const titleFilter = event.target.value.toLowerCase();
-    const filteredMovies = movies.filter(movie => movie.title.toLowerCase().includes(titleFilter));
+    const filteredMovies = movies.filter((movie) =>
+      movie.title.toLowerCase().includes(titleFilter)
+    );
     setFilteredMovies(filteredMovies);
   };
 
   const handleRatingChange = (event) => {
     const ratingFilter = parseFloat(event.target.value);
-    const filteredMovies = movies.filter(movie => movie.rating >= ratingFilter);
+    const filteredMovies = movies.filter(
+      (movie) => movie.rating >= ratingFilter
+    );
     setFilteredMovies(filteredMovies);
   };
 
   return (
     <div className="app">
-      <Filter onTitleChange={handleTitleChange} onRatingChange={handleRatingChange} />
+      <Filter
+        onTitleChange={handleTitleChange}
+        onRatingChange={handleRatingChange}
+      />
       <MovieList movies={filteredMovies} />
     </div>
   );
